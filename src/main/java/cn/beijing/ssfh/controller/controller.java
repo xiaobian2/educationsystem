121795685f365a845b62319f8dbdebceb0eef7cc
viphone <18516966845@163.com>
2017-09-23 21:16:37 +0800

    Initial commit

---
+++ src/main/java/cn/beijing/ssfh/controller/controller.java
@@ -0,0 +1,7 @@
+package cn.beijing.ssfh.controller;
+
+/**
+ * Created by Mr.W on 2017/9/23.
+ */
+public class controller {
+}
---
+++ src/main/java/cn/beijing/ssfh/pojo/pojo.java
@@ -0,0 +1,7 @@
+package cn.beijing.ssfh.pojo;
+
+/**
+ * Created by Mr.W on 2017/9/23.
+ */
+public class pojo {
+}
